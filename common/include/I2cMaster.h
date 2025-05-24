958ec512005aa7132ed2252fe5325b9335c7df75
Alexandru Puscasu <palexandru2000@gmail.com>
2025-05-24 11:59:38 +0300

    add doxygen comments

--- common/include/I2cMaster.h
+++ common/include/I2cMaster.h
@@ -3,24 +3,46 @@
 
 #include <laboratory_types.h>
 
+/// @brief Implement high level functions for I2C protocol, in master mode, in ATmega328p
 class I2cMaster {
 protected:
+    /// @brief Send one byte over I2C
+    /// @param data The byte to be send
     void _writeByte(const char& data);
 
+    /// @brief Read one byte from I2C
+    /// @param data The response byte will be in this variable
     void _readByte(char& data);
 public:
+    /// @brief This function enable and configure I2C peripheral. Also configure MCU pins
     void init();
 
+    /// @brief Send start condition for I2C
     void sendStart();
 
+    /// @brief Send slave address plus write bit
+    /// @param addr The slave address
+    /// @return True if ACK bit was returned
     bool writeAddrWrite(const char& addr);
 
+    /// @brief Send slave address plus read bit
+    /// @param addr The slave address
+    /// @return True if ACK bit was returned
     bool writeAddrRead(const char& addr);
 
+    /// @brief Send one byte over I2C, after addressing stage
+    /// @param d Data byte to be send
+    /// @param expectedResponse The expected response
+    /// @return True if response is equal with expected response
     bool writeByte(const char& d, I2cResponse expectedResponse = ACKNOWLEDGE);
 
+    /// @brief Read one byte from I2C, after addressing stage
+    /// @param d The response will be in this variable
+    /// @param expectedResponse The expected response
+    /// @return True if response is equal with expected response
     bool readByte(char& d, I2cResponse expectedResponse = ACKNOWLEDGE);
 
+    /// @brief Send stop condition for I2C
     void sendStop();
 };
 
--- common/include/Uart.h
+++ common/include/Uart.h
@@ -1,18 +1,31 @@
 #ifndef UART_H
 #define UART_H
 
+/// @brief Implement high level functions for UART protocol in ATmega328p
 class Uart {
 public: 
+    /// @brief This function enable and configure UART peripheral. Also configure MCU pins
     void init();
 
+    /// @brief Write one byte on TX chanel, lock until send ends
+    /// @param d The 8 bits will be send over UART
     void writeByte(const char& d);
 
+    /// @brief Check if UART RX buffer has data
+    /// @return If there are data in RX buffer
     bool available();
 
+    /// @brief Take one data from RX buffer, lock until data available
+    /// @return One byte (8 bits) from UART RX buffer
     char readByte();
 
+    /// @brief Send a string over UART TX chanel
+    /// @param msg The message to be send
     void writeString(const char* msg);
 
+    /// @brief Receive an integer number, convert to string and send over UART TX
+    /// @param number The integer number to be send
+    /// @param base The radix in which the number will be represented
     void writeIntegerNumber(const int& number, const int& base = 10);
 };
 
